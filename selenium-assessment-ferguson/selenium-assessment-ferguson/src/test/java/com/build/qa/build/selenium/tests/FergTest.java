a492dcd332634b699d35a0c37846e098e5aa34c3
geethasm683 <107883883+geethasm683@users.noreply.github.com>
2022-06-22 00:17:47 +0530

    Add files via upload

---
+++ selenium-assessment-ferguson/selenium-assessment-ferguson/src/test/java/com/build/qa/build/selenium/tests/FergTest.java
@@ -0,0 +1,77 @@
+package com.build.qa.build.selenium.tests;
+
+import org.junit.Test;
+
+import com.build.qa.build.selenium.framework.BaseFramework;
+import com.build.qa.build.selenium.pageobjects.homepage.HomePage;
+
+public class FergTest extends BaseFramework {
+
+	/**
+	 * Extremely basic test that outlines some basic
+	 * functionality and page objects as well as assertJ
+	 */
+	@Test
+	public void navigateToHomePage() {
+		driver.get(getConfiguration("HOMEPAGE"));
+		HomePage homePage = new HomePage(driver, wait);
+
+		softly.assertThat(homePage.onHomePage())
+			.as("The website should load up with the Build.com desktop theme.")
+			.isTrue();
+	}
+
+	/**
+	 * Search for the Moen m6702bn from the search bar
+	 * @assert: That the product page we land on is what is expected by checking the product brand and product id
+	 * @difficulty Easy
+	 */
+	@Test
+	public void searchForProductLandsOnCorrectProduct() {
+		HomePage.Close(driver).click();
+		HomePage.SearchOption(driver).sendKeys("Moen m6702bn");
+		HomePage.SearchOptionClick(driver).click();
+	}
+
+	/**
+	 * Go to the Bathroom Sinks category directly
+	 * (https://www.ferguson.com/category/bathroom-plumbing/bathroom-faucets/bathroom-sink-faucets/_/N-zbq4i3)
+	 * and add the second product on the search results (Category Drop) page to the cart.
+	 * @assert: the product that is added to the cart is what is expected
+	 * @difficulty Easy-Medium
+	 */
+	@Test
+	public void addProductToCartFromCategoryDrop() throws InterruptedException {
+		HomePage.BathRoomLink(driver).click();
+		Thread.sleep(1000);
+		HomePage.BathRoomSinkLink(driver).click();
+	}
+
+	/**
+	 * Add two different finishes of a product (such as Moen m6702bn) to cart,
+	 * change the quantity of each finish on the cart page
+	 * @assert that the product and cart total update as expected when the quantity is changed
+	 * @difficulty Medium-Hard
+	 */
+	@Test
+	public void addMultipleCartItemsAndChangeQuantity() {
+		HomePage.BathRoomCart(driver).click()
+		;
+	}
+
+	/**
+	 * Go to a category drop page (such as Bathroom Faucets) and narrow by
+	 * at least two filters (facets), e.g: Finish=Chromes and Brand=Brizo
+	 * @assert that the correct filters are being narrowed, and the result count
+	 * is correct, such that each facet selection is narrowing the product count.
+	 * @difficulty Hard
+	 */
+	@Test
+	public void facetNarrowBysResultInCorrectProductCounts() throws InterruptedException {
+		HomePage.BathRoomFaucteLink(driver).click();
+		Thread.sleep(1000);
+		HomePage.BathSingel(driver).click();
+		HomePage.Facest(driver).click();
+
+	}
+}
